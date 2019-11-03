2cfc73d132872f59c564516885247df4168e39ed
Gonza Hirsch <hirschgonzalo@yahoo.com.ar>
2019-11-03 12:19:21 -0300

    Updated solutions

---
+++ try2open.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <error.h>
+
+int main(int argc, char const *argv[]) 
+{ 
+    while(1){
+        
+    }
+    
+    return 0; 
+} 
