de0c276140e254131a496466119ac95d6463e573
Gonza Hirsch <hirschgonzalo@yahoo.com.ar>
2019-11-05 20:29:07 -0300

    Minor changes

--- client.c
+++ client.c
@@ -1,60 +1,65 @@
-// Client side C/C++ program to demonstrate Socket programming 
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <unistd.h> 
-#include <string.h> 
-#define PORT 23423 
-   
-int main(int argc, char const *argv[]) 
-{ 
-    //printf("\n# \033[D \033[A \033[A \033[D \033[B \033[C \033[B \033[D *\n");
-    int sock = 0, valread; 
-    struct sockaddr_in serv_addr; 
-    char *hello = "quine"; 
-    char buffer[1024] = {0}; 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
-    { 
-        printf("\n Socket creation error \n"); 
-        return -1; 
-    } 
-   
-    serv_addr.sin_family = AF_INET; 
-    serv_addr.sin_port = htons(PORT); 
-       
-    // Convert IPv4 and IPv6 addresses from text to binary form 
-    if(inet_pton(AF_INET, "0.0.0.0", &serv_addr.sin_addr)<=0)  
-    { 
-        printf("\nInvalid address/ Address not supported \n"); 
-        return -1; 
-    } 
-   
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-    { 
-        printf("\nConnection Failed \n"); 
-        return -1; 
-    } 
+// Client side C/C++ program to demonstrate Socket programming
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#define PORT 8085
+#define MAX_MESSAGE_LENGTH 1024
 
-    //send(sock , hello , strlen(hello) , 0 );
+int main(int argc, char const *argv[])
+{
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
 
-    char buff[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, "0.0.0.0", &serv_addr.sin_addr)<=0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    char buff[MAX_MESSAGE_LENGTH] = {0};
     char c;
-    int index = 0;
+    int index;
 
     while(strcmp(buff, "quit") != 0){
-        printf( "Enter a command :");
-        while((c = getchar()) != '\n'){
-            buff[index++] = c;
+        bzero(buff, MAX_MESSAGE_LENGTH);
+        index = 0;
+        printf( "\nEnter a message: ");
+
+        while ((buff[index++] = getchar()) != '\n' && index < MAX_MESSAGE_LENGTH);
+
+        printf( "Message length %d", index);
+        if (index >= MAX_MESSAGE_LENGTH){
+          printf( "\nThe message is too long!");
+        } else {
+          buff[index] = 0;
+
+          write(sock, buff, sizeof(buff));
         }
-        buff[index++] = '\n';
-        buff[index] = 0;
 
         //gets(buff);
-        send(sock , buff , strlen(buff) , 0 ); 
-        //valread = read( sock , buffer, 1024); 
-        //printf("%s\n",buffer ); 
-        index = 0;
-    }    
+        //send(sock , buff , strlen(buff) , 0 );
+        //valread = read( sock , buffer, 1024);
+        //printf("%s\n",buffer );
 
-    return 0; 
-} 
+    }
+
+    return 0;
+}
--- our_server.c
+++ our_server.c
@@ -5,90 +5,109 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#define MAX 128
-#define PORT 23423
+#include <sys/ioctl.h>
+#define MAX_MESSAGE_LENGTH 1024
+#define PORT 8085
 #define SA struct sockaddr
+#define MAX_LEVEL 10
 
-// Function designed for chat between client and server.
-void func(int sockfd)
-{
-    char buff[MAX];
-    int n;
-    // infinite loop for chat
-    for (;;) {
-        bzero(buff, MAX);
+static int level = 0;
 
-        // read the message from client and copy it in buffer
-        read(sockfd, buff, sizeof(buff));
-        // print buffer which contains the client contents
-        printf("From client: %s\t To client : ", buff);
-        bzero(buff, MAX);
-        n = 0;
-        // copy server message in the buffer
-        while ((buff[n++] = getchar()) != '\n')
-            ;
+int init_server(){
+  int sockfd, connfd, len, res;
+  struct sockaddr_in servaddr, cli;
 
-        // and send that buffer to client
-        write(sockfd, buff, sizeof(buff));
+  // Crea el socket y verifica que la creacion haya sido correcta
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd == -1) {
+      printf("socket creation failed...\n");
+      perror("Error");
+      exit(0);
+  }
+  else {
+      printf("Socket successfully created..\n");
+  }
 
-        // if msg contains "Exit" then server exit and chat ended.
-        if (strncmp("exit", buff, 4) == 0) {
-            printf("Server Exit...\n");
-            break;
-        }
-    }
+  // Borra los contenidos de lo que hay en servaddr
+  bzero(&servaddr, sizeof(servaddr));
+
+  // Asinga el IP y el PUERTO
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  servaddr.sin_port = htons(PORT);
+
+  // Bindea el soket al IP y verifica que sea correcto eso
+  if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
+      printf("socket bind failed...\n");
+      perror("Error");
+      exit(0);
+  }
+  else
+      printf("Socket successfully binded..\n");
+
+  // Verifica que el server pueda escuchar
+  if ((listen(sockfd, 5)) != 0) {
+      printf("Listen failed...\n");
+      perror("Error");
+      exit(0);
+  }
+  else {
+      printf("Server listening..\n");
+  }
+
+  len = sizeof(cli);
+
+  // Espera a que el client se conecte y verifica
+  connfd = accept(sockfd, (SA*)&cli, &len);
+  if (connfd < 0) {
+      printf("server acccept failed...\n");
+      perror("Error");
+      exit(0);
+  }
+  else {
+      printf("server acccept the client...\n");
+      level++;
+  }
+
+  return connfd;
 }
 
-// Driver function
+void start_game(int socket_fd){
+  char buff[MAX_MESSAGE_LENGTH];
+  int read_status;
+
+  while(level < MAX_LEVEL && level > 0){
+    // Pone en cero al buffer que recibe mensajes
+    bzero(buff, MAX_MESSAGE_LENGTH);
+
+    read_status = read(socket_fd, buff, sizeof(buff));
+    if (read_status >= 0){
+      printf("Message from client: %s", buff);
+
+      // TODO: USAR LA RESPUESTA
+      switch (level)
+      {
+          case 1:
+              break;
+          case 2:
+              break;
+      }
+    }
+    // read the message from client and copy it in buffer
+  }
+}
+
 int main()
 {
-    int sockfd, connfd, len;
-    struct sockaddr_in servaddr, cli;
 
-    // socket create and verification
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        printf("socket creation failed...\n");
-        exit(0);
-    }
-    else
-        printf("Socket successfully created..\n");
-    bzero(&servaddr, sizeof(servaddr));
+    level = 0;
 
-    // assign IP, PORT
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(PORT);
+    // Inicializa el socket y la conexion
+    int socket_fd = init_server();
 
-    // Binding newly created socket to given IP and verification
-    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
-        printf("socket bind failed...\n");
-        exit(0);
-    }
-    else
-        printf("Socket successfully binded..\n");
+    // Empieza el juego
+    start_game(socket_fd);
 
-    // Now server is ready to listen and verification
-    if ((listen(sockfd, 5)) != 0) {
-        printf("Listen failed...\n");
-        exit(0);
-    }
-    else
-        printf("Server listening..\n");
-    len = sizeof(cli);
-
-    // Accept the data packet from client and verification
-    connfd = accept(sockfd, (SA*)&cli, &len);
-    if (connfd < 0) {
-        printf("server acccept failed...\n");
-        exit(0);
-    }
-    else
-        printf("server acccept the client...\n");
-
-    // Function for chatting between client and server
-    func(connfd);
-
-    // After chatting close the socket
-    close(sockfd);
+    // Cierra el socket al terminar
+    close(socket_fd);
 }
