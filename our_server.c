1829b95d49498b8a7845a0840a2329391123245e
Gonza Hirsch <hirschgonzalo@yahoo.com.ar>
2019-11-07 10:35:00 -0300

    Done Quine

--- our_server.c
+++ our_server.c
@@ -42,7 +42,7 @@ static char * rsp[MAX_LEVEL] = {
   "indeterminado",
   "la gioconda",
   "",// Alphabeto Fonetico Nato -> Se genera solo
-  "",
+  "abalastro",
   "gdb es la hostia"
 };
 
@@ -57,7 +57,7 @@ static char * pistas[MAX_LEVEL] = {
   "mixed fds",
   "Portrait",
   "",// Alphabeto Fonetico Nato -> Se genera solo
-  "",
+  "quine",
   "b gdbme y encontrá el valor mágico"
 };
 
@@ -76,6 +76,7 @@ void gen_c();
 int init_server();
 void start_game(int socket_fd);
 void say_hi();
+void quine();
 
 void portrait(){
   puts("                                  _______                                                                 _,,ad8888888888bba,_                                                        ,ad88888I888888888888888ba,                                                  ,88888888I88888888888888888888a,                                             ,d888888888I8888888888888888888888b,                                          d88888PP\"\"\"\" \"\"YY88888888888888888888b,                                      ,d88\"\'__,,--------,,,,.;ZZZY8888888888888,                                    ,8IIl\'\"                ;;l\"ZZZIII8888888888,                                  ,I88l;\'                  ;lZZZZZ888III8888888,                               ,II88Zl;.                  ;llZZZZZ888888I888888,                             ,II888Zl;.                .;;;;;lllZZZ888888I8888b                            ,II8888Z;;                 `;;;;;\'\'llZZ8888888I8888,                           II88888Z;\'                        .;lZZZ8888888I888b                           II88888Z; _,aaa,      .,aaaaa,__.l;llZZZ88888888I888                           II88888IZZZZZZZZZ,  .ZZZZZZZZZZZZZZ;llZZ88888888I888,                          II88888IZZ<\'(@@>Z|  |ZZZ<\'(@@>ZZZZ;;llZZ888888888I88I                         ,II88888;   `\"\"\" ;|  |ZZ; `\"\"\"     ;;llZ8888888888I888                         II888888l            `;;          .;llZZ8888888888I888,                       ,II888888Z;           ;;;        .;;llZZZ8888888888I888I                       III888888Zl;    ..,   `;;       ,;;lllZZZ88888888888I888                       II88888888Z;;...;(_    _)      ,;;;llZZZZ88888888888I888,                      II88888888Zl;;;;;\' `--\'Z;.   .,;;;;llZZZZ88888888888I888b                      ]I888888888Z;;;;\'   \";llllll;..;;;lllZZZZ88888888888I8888,                     II888888888Zl.;;\"Y88bd888P\";;,..;lllZZZZZ88888888888I8888I                     II8888888888Zl;.; `\"PPP\";;;,..;lllZZZZZZZ88888888888I88888                     II888888888888Zl;;. `;;;l;;;;lllZZZZZZZZW88888888888I88888                     `II8888888888888Zl;.    ,;;lllZZZZZZZZWMZ88888888888I88888                      II8888888888888888ZbaalllZZZZZZZZZWWMZZZ8888888888I888888,                     `II88888888888888888b\"WWZZZZZWWWMMZZZZZZI888888888I888888b                      `II88888888888888888;ZZMMMMMMZZZZZZZZllI888888888I8888888                       `II8888888888888888 `;lZZZZZZZZZZZlllll888888888I8888888,                       II8888888888888888, `;lllZZZZllllll;;.Y88888888I8888888b,                     ,II8888888888888888b   .;;lllllll;;;.;..88888888I88888888b,                    II888888888888888PZI;.  .`;;;.;;;..; ...88888888I8888888888,                   II888888888888PZ;;\';;.   ;. .;.  .;. .. Y8888888I88888888888b,                ,II888888888PZ;;\'                        `8888888I8888888888888b,              II888888888\'                              888888I8888888888888888b            ,II888888888                              ,888888I88888888888888888           ,d88888888888                              d888888I8888888888ZZZZZZZ        ,ad888888888888I                              8888888I8888ZZZZZZZZZZZZZ      ,d888888888888888\'                              888888IZZZZZZZZZZZZZZZZZZ    ,d888888888888P\'8P\'                               Y888ZZZZZZZZZZZZZZZZZZZZZ   ,8888888888888,  \"                                 ,ZZZZZZZZZZZZZZZZZZZZZZZZ  d888888888888888,                                ,ZZZZZZZZZZZZZZZZZZZZZZZZZZZ  888888888888888888a,      _                    ,ZZZZZZZZZZZZZZZZZZZZ888888888  888888888888888888888ba,_d\'                  ,ZZZZZZZZZZZZZZZZZ88888888888888  8888888888888888888888888888bbbaaa,,,______,ZZZZZZZZZZZZZZZ888888888888888888  88888888888888888888888888888888888888888ZZZZZZZZZZZZZZZ888888888888888888888  8888888888888888888888888888888888888888ZZZZZZZZZZZZZZ88888888888888888888888  888888888888888888888888888888888888888ZZZZZZZZZZZZZZ888888888888888888888888  8888888888888888888888888888888888888ZZZZZZZZZZZZZZ88888888888888888888888888  88888888888888888888888888888888888ZZZZZZZZZZZZZZ8888888888888888888888888888  8888888888888888888888888888888888ZZZZZZZZZZZZZZ88888888888888888888888888888  88888888888888888888888888888888ZZZZZZZZZZZZZZ8888888888888888888888888888888  8888888888888888888888888888888ZZZZZZZZZZZZZZ88888888888888888888888888888888");
@@ -341,6 +342,45 @@ void gen_c(){
   nato_pa(pistas[9], rsp[9]);
 }
 
+void quine(){
+  char * command = "gcc quine.c -o quine | grep -c \"error\"";
+  char buff[1024] = {0};
+
+  FILE *fp;
+
+  if ((fp = popen(command, "r")) == NULL) {
+      printf("Error opening pipe!\n");
+      return;
+  }
+  while (fgets(buff, 1024, fp) != NULL);
+  if(pclose(fp) < 0)  {
+      printf("Command not found or exited with error status\n");
+      return;
+  }
+
+  if (strcmp(buff, "0\n") == 0){
+    printf("Muy bien, ya metieron un archivo! Veamos si hace lo que deberia!\n");
+    command = "./quine | diff - quine.c";
+    memset(buff, 0, sizeof(buff));
+    if ((fp = popen(command, "r")) == NULL) {
+        printf("Error opening pipe!\n");
+        return;
+    }
+    while (fgets(buff, 1024, fp) != NULL);
+    if (strcmp(buff, "") == 0){
+        printf("Genial! La respuesta a este ejercicio es abalastro");
+    } else {
+        printf("Diff Returned: %s", buff);
+    }
+    if(pclose(fp) < 0){
+        printf("Command not found or exited with error status\n");
+        return;
+    }
+  } else {
+    printf("El archivo no está o no es correcto.");
+  }
+}
+
 int init_server(){
   int sockfd, connfd, len, res;
   struct sockaddr_in servaddr, cli;
@@ -417,9 +457,11 @@ void start_game(int socket_fd){
     say_hi();
     printf("%s\n", desafio_header);
     printf("%s\n", pistas[level]);
-    fflush( stdout );
 
     switch(level){
+      case 1:
+        quine();
+      break;
       case 4:
         ebadf();
       break;
@@ -430,9 +472,13 @@ void start_game(int socket_fd){
         portrait();
       break;
       case 10:
+        quine();
+      break;
+      case 11:
         gdbme();
       break;
     }
+    fflush( stdout );
 
     read(socket_fd, buff, sizeof(buff));
     if (strcmp(rsp[level], buff) == 0){
